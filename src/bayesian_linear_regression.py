Fixes bounds overflow due to floating point precision

src/bayesian_linear_regression.py
@@ -92,6 +92,7 @@ class BayesianLinearRegression(object):
                 res = optimize.fmin(self.negative_mll, np.random.rand(2), disp=False)
                 self.alpha = np.exp(res[0])
                 self.beta = np.exp(res[1])
+                self._current_thetas = [self]
 
         S_inv = self.beta * np.dot(self.X.T, self.X) + np.eye(self.X.shape[1]) * self.alpha
 
@@ -103,13 +104,20 @@ class BayesianLinearRegression(object):
         self.S = S
 
     def predict_all(self, X):
-        return np.array([self.predict(X)])
+        num_X = X.shape[0]
+        predictions = np.zeros((len(self._current_thetas), 2, num_X))
+        for i, theta in enumerate(self._current_thetas):
+            gp = theta
+            mean, var = gp.predict(X)
+            predictions[i, 0, :] = mean[:,0]
+            predictions[i, 1, :] = var[:,0]
+        return predictions
 
     def predict(self, X, theta=None):
         if theta is not None:
             model = theta
             return model.predict(X)
-
+ 
         m = np.dot(self.m.T, X.T)
         v = np.diag(np.dot(np.dot(X, self.S), X.T)) + 1. / self.beta
         m = m.T
@@ -117,49 +125,62 @@ class BayesianLinearRegression(object):
         v = v[:, None]
         return m, v
 
-# class GPyLinearRegression(GPyRegression):
-#     def __init__(**kwargs):
-#         super().__init__(kernel=GPy.kern.Linear(dim_basis), **kwargs)
 
-# class GPyRegression(object):
-#     def __init__(self, kernel, num_mcmc=0, do_optimize=True):
-#         self._current_thetas = None
+class GPyRegression(object):
+    def __init__(self, kernel, num_mcmc=0, do_optimize=True, noise_prior=None, fix_noise=False):
+        self.gp = None
+        self._current_thetas = None
 
-#         # If 0 max point estimate is used via max likelihood.
-#         self.num_mcmc = num_mcmc
-#         self.do_optimize = do_optimize
-#         self.kernel = kernel
+        # If 0 max point estimate is used via max likelihood.
+        self.num_mcmc = num_mcmc
+        self.do_optimize = do_optimize
+        self.kernel = kernel
+        self.noise_prior = noise_prior
+        self.fix_noise = fix_noise
 
-#     def fit(self, X, y):
-#         dim_basis = X.shape[1]
-#         self.gp = GPy.models.GPRegression(X, y, self.kernel)
+    def fit(self, X, y):
+        dim_basis = X.shape[1]
 
-#         # Set hyperpriors
-#         hyperprior = GPy.priors.Gamma.from_EV(0.5, 1)
-#         self.kernel.variances.set_prior(hyperprior) # log_prior()
-#         self.gp.Gaussian_noise.variance.set_prior(hyperprior)
+        if self.gp is None:
+            self.gp = GPy.models.GPRegression(X, y, self.kernel)
 
-#         # Optimize
-#         if self.do_optimize:
-#             if self.num_mcmc > 0:
-#                 # Most likely hyperparams given data
-#                 hmc = GPy.inference.mcmc.HMC(self.gp)
-#                 hmc.sample(num_samples=2000) # Burn-in
-#                 self._current_thetas = hmc.sample(num_samples=2, hmc_iters=50)
-#             else:
-#                 self.gp.randomize()
-#                 self.gp.optimize()
-#                 self._current_thetas = [self.gp.param_array]        
+            if self.noise_prior:
+                self.gp.Gaussian_noise.variance.set_prior(self.noise_prior)
+            
+            if self.fix_noise:
+                self.gp.Gaussian_noise.fix(0)
+        else:
+            self.gp.set_XY(X, y)
 
-#     def predict_all(self, X):
-#         predictions = np.zeros((len(self._current_thetas), 2))
-#         for i, theta in enumerate(self._current_thetas):
-#             self.gp[:] = self._current_thetas[0]
-#             predictions[i, :] = self.gp.predict(X)
-#         return predictions
+        # Optimize
+        if self.do_optimize:
+            if self.num_mcmc > 0:
+                # Most likely hyperparams given data
+                hmc = GPy.inference.mcmc.HMC(self.gp)
+                hmc.sample(num_samples=1000) # Burn-in
+                self._current_thetas = hmc.sample(num_samples=self.num_mcmc, hmc_iters=50)
+            else:
+                self.gp.randomize()
+                self.gp.optimize()
+                self._current_thetas = [self.gp.param_array]        
 
-#     def predict(self, X, theta=None):
-#         # TODO: use aggregate of samples
-#         if theta is not None:
-#             self.gp[:] = theta
-#         return self.gp.predict(X)
+    def predict_all(self, X):
+        num_X = X.shape[0]
+        predictions = np.zeros((len(self._current_thetas), 2, num_X))
+        for i, theta in enumerate(self._current_thetas):
+            self.gp[:] = theta
+            mean, var = self.gp.predict(X)
+            predictions[i, 0, :] = mean[:,0]
+            predictions[i, 1, :] = var[:,0]
+        return predictions
+
+    def predict(self, X, theta=None):
+        # TODO: use aggregate of samples
+        if theta is not None:
+            self.gp[:] = theta
+        return self.gp.predict(X)
+
+
+class GPyLinearRegression(GPyRegression):
+    def __init__(self, input_dim=1, **kwargs):
+        super(GPyLinearRegression).__init__(kernel=GPy.kern.Linear(input_dim), **kwargs)

src/bo.py
@@ -38,6 +38,14 @@ def random_hypercube_samples(n_samples, bounds):
     samples = a * np.abs(bounds_repeated[:,1] - bounds_repeated[:,0]) + bounds_repeated[:,0]
     return np.swapaxes(samples, 0, 1)
 
+
+def constrain_point(x, bounds):
+    minx = bounds[:, 0]
+    maxx = bounds[:, 1]
+    x = np.maximum(x, minx)
+    return np.minimum(x, maxx)
+
+
 class BO(object):
     def __init__(self, obj_func, model, acquisition_function=None, n_iter = 10, bounds=np.array([[0,1]])):
         self.n_iter = n_iter
@@ -62,6 +70,10 @@ class BO(object):
 
         # TODO: turn into numpy operations to parallelize
         for x0 in random_hypercube_samples(n_starts, self.bounds):
+            # This handles the case where the sample is slightly above or below the bounds
+            # due to floating point precision.
+            x0 = constrain_point(x0, self.bounds)
+            
             res = minimize(min_obj, x0=x0, bounds=self.bounds, method='L-BFGS-B')        
             if res.fun < min_y:
                 min_y = res.fun
@@ -199,6 +211,7 @@ class BO(object):
         if do_plot:
             self.plot_prediction()
 
-        # TODO: Move session outside..         
-        self.model.sess.close()
-        self.model.sess = None
+        # TODO: Move session outside..
+        if hasattr(self.model, "sess"):
+            self.model.sess.close()
+            self.model.sess = None

src/dngo.py
@@ -12,11 +12,44 @@ from scipy.optimize import minimize
 from scipy import optimize
 from scipy.stats import multivariate_normal
 
-from .bayesian_linear_regression import BayesianLinearRegression
+from .bayesian_linear_regression import BayesianLinearRegression, GPyRegression
 from .neural_network import zero_mean_unit_var_unnormalization
 from .normalization import zero_mean_unit_var_normalization, zero_mean_unit_var_unnormalization
 
-class BOModel(object):
+class BOBaseModel(object):
+    def get_incumbent(self):
+        i = np.argmax(self.Y)
+        return self.X[i], self.Y[i]
+
+    def init(self, X, Y, **kwargs):
+        self.X = X
+        self.Y = Y
+        self.fit(self.X, self.Y, **kwargs)
+
+    def add_observations(self, X_new, Y_new, **kwargs):
+        # Update data
+        self.X = np.concatenate([self.X, X_new])
+        self.Y = np.concatenate([self.Y, Y_new])
+        
+        self.fit(self.X, self.Y, **kwargs)
+
+    def plot_acq(self, X_line, acq):
+        plt.plot(X_line, self.acq(X_line, acq), color="red")
+
+    def fit(self, X, Y): 
+        raise NotImplementedError
+
+    def acq(self, X, acq):
+        raise NotImplementedError
+
+    def predict(self, X):
+        raise NotImplementedError
+
+    def plot_prediction(self, X_line, Y_line, x_new=None):
+        raise NotImplementedError
+
+
+class BOModel(BOBaseModel):
     # def __enter__(self)
     # def __exit__(self, exc_type, exc_value, tracepck)
 
@@ -39,18 +72,6 @@ class BOModel(object):
         else:
             self.gp = regressor
 
-    def init(self, X, Y, train_nn=True):
-        self.X = X
-        self.Y = Y
-        self.fit(self.X, self.Y, train_nn=train_nn)
-
-    def add_observations(self, X_new, Y_new, train_nn=True):
-        # Update data
-        self.X = np.concatenate([self.X, X_new])
-        self.Y = np.concatenate([self.Y, Y_new])
-        
-        self.fit(self.X, self.Y, train_nn=train_nn)
-
     def fit(self, X, Y, train_nn=True):
         if self.normalize_input:
             self.transformed_X, self.X_mean, self.X_std = zero_mean_unit_var_normalization(X)
@@ -69,19 +90,17 @@ class BOModel(object):
 
         self.gp.fit(self.transformed_D, self.transformed_Y)
 
-    def get_incumbent(self):
-        i = np.argmax(self.Y)
-        return self.X[i], self.Y[i]
-
     def acq(self, X, acq):
         """Note prediction is done in normalized space.
         """
 
         D = self.predict_basis(X)
+        # Note: no need to denormalize.
         transformed_sample_predictions = self.gp.predict_all(D)
 
+        asd = np.array([acq(pred[0, :], pred[1, :]) for pred in transformed_sample_predictions])
         # Average over all sampled hyperparameter predictions
-        return np.average(np.array([acq(mean, var) for (mean, var) in transformed_sample_predictions]), axis=0)
+        return np.average(asd, axis=0)
 
     def predict_basis(self, X):
         if self.normalize_input:
@@ -101,9 +120,6 @@ class BOModel(object):
     def predict(self, X):
         D = self.predict_basis(X)
         return self.predict_from_basis(D)
-
-    def plot_acq(self, X_line, acq):
-        plt.plot(X_line, self.acq(X_line, acq), color="red")
 
     def plot_prediction(self, X_line, Y_line, x_new=None):
         D_line = self.predict_basis(X_line)
@@ -125,66 +141,64 @@ class BOModel(object):
         plt.plot(X_line, Y_line, dashes=[2, 2], color='black')
 
 
-# class BOModel(object):
-#     def __init__(self, nn, kernel, num_mcmc=0, regressor=None):
-#         self.gp = GPyRegression(kernel, num_mcmc=0)
+class GPyBOModel(BOBaseModel):
+    def __init__(self, kernel, normalize_input=True, normalize_output=True, **kwargs):
+        self.X_mean = None
+        self.X_std = None
+        self.normalize_input = normalize_input
 
-#     def init(self, X, Y, train_nn=True):
-#         self.X = X
-#         self.Y = Y
+        self.y_mean = None
+        self.y_std = None
+        self.normalize_output = normalize_output
 
-#         self.gp.fit(self.D, self.nn_model.y)
+        self.gp = GPyRegression(kernel, **kwargs)
 
-#     def add_observations(self, X_new, Y_new):
-#         # Update data
-#         self.X = np.concatenate([self.X, X_new])
-#         self.Y = np.concatenate([self.Y, Y_new])
+    def fit(self, X, Y):
+        if self.normalize_input:
+            self.transformed_X, self.X_mean, self.X_std = zero_mean_unit_var_normalization(X)
+        else:
+            self.transformed_X = X
 
-#         # Fit BLR
-#         self.gp.fit(self.D, self.nn_model.y)
+        if self.normalize_output:
+            self.transformed_Y, self.y_mean, self.y_std = zero_mean_unit_var_normalization(Y)
+        else:
+            self.transformed_Y = Y
 
-#     def get_incumbent(self):
-#         i = np.argmax(self.Y)
-#         return self.X[i], self.Y[i]
+        self.gp.fit(self.transformed_X, self.transformed_Y)
 
-#     def acq(self, X, acq):
-#         """Note prediction is done in normalized space.
-#         """
+    def acq(self, X, acq):
+        """Note prediction is done in normalized space.
+        """
 
-#         D = self.nn_model.predict_basis(self.sess, X)
-#         sample_predictions = self.gp.predict_all(D)
+        if self.normalize_input:
+            X, _, _ = zero_mean_unit_var_normalization(X, mean=self.X_mean, std=self.X_std)
+        transformed_sample_predictions = self.gp.predict_all(X)
 
-#         # Average over all sampled hyperparameter predictions
-#         return np.average(np.array([acq(mean, var) for (mean, var) in sample_predictions]), axis=0)
+        asd = np.array([acq(pred[0, :], pred[1, :]) for pred in transformed_sample_predictions])
+        # Average over all sampled hyperparameter predictions
+        return np.average(asd, axis=0)
 
-#     def predict(self, X):
-#         D = self.nn_model.predict_basis(self.sess, X)
-#         return self.predict_from_basis(D)
+    def predict(self, X, theta=None):
+        if self.normalize_input:
+            X, _, _ = zero_mean_unit_var_normalization(X, mean=self.X_mean, std=self.X_std)
 
-#     def plot_acq(self, X_line, acq):
-#         plt.plot(X_line, self.acq(X_line, acq), color="red")
+        mean, var = self.gp.predict(X, theta=theta)
+        
+        if self.normalize_output is not None:
+            mean = zero_mean_unit_var_unnormalization(mean, self.y_mean, self.y_std)
+            var = var * self.y_std ** 2
 
-#     def plot_prediction(self, X_line, Y_line, x_new=None):
-#         D_line = self.nn_model.predict_basis(self.sess, X_line)
+        return mean, var
 
-#         if self.gp.num_mcmc > 0:
-#             for theta in self.gp._current_thetas:
-#                 mean, var = self.predict_from_basis(D_line, theta=theta)
-#                 plt.fill_between(X_line.reshape(-1), (mean + np.sqrt(var)).reshape(-1), (mean - np.sqrt(var)).reshape(-1), alpha=.2)
-#                 plt.plot(X_line, mean)
-#         else:
-#             mean, var = self.predict_from_basis(D_line)
-#             plt.fill_between(X_line.reshape(-1), (mean + np.sqrt(var)).reshape(-1), (mean - np.sqrt(var)).reshape(-1), alpha=.2)
-#             plt.plot(X_line, mean)
-            
+    def plot_prediction(self, X_line, Y_line, x_new=None):
+        for theta in self.gp._current_thetas:
+            mean, var = self.predict(X_line, theta=theta)
+            plt.fill_between(X_line.reshape(-1), (mean + np.sqrt(var)).reshape(-1), (mean - np.sqrt(var)).reshape(-1), alpha=.2)
+            plt.plot(X_line, mean)
 
-#         if x_new is not None:
-#             plt.axvline(x=x_new, ls='--', c='k', lw=1, label='Next sampling location')
+        if x_new is not None:
+            plt.axvline(x=x_new, ls='--', c='k', lw=1, label='Next sampling location')
 
-#         # TODO: remember to normalize if normalization is pulled out into BOModel
-#         plt.scatter(self.X, self.Y)
-#         plt.plot(X_line, Y_line, dashes=[2, 2], color='black')
-#         # plt.plot(X_line, self.acq(X_line), color='red')
-
-#     def __del__(self):
-#         self.sess.close()
+        # TODO: remember to normalize if normalization is pulled out into BOModel
+        plt.scatter(self.X, self.Y)
+        plt.plot(X_line, Y_line, dashes=[2, 2], color='black')

src/tests.py
@@ -1,9 +1,13 @@
 from functools import wraps
+from operator import itemgetter
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pathos.multiprocessing as mp
+from hpolib.benchmarks.synthetic_functions import Branin
 
-from src.bo import random_hypercube_samples, vectorize
+from .bo import random_hypercube_samples, vectorize
 
 def immidiate_regret(y, y_opt):
     return np.abs(y - y_opt)
@@ -44,12 +48,30 @@ def test_random_sample(f, bounds, n_iter=100):
     return R_samples, R_values
 
 
+def test_gp(f, bounds, n_iter, do_plot=False):
+    import GPy
+    from .dngo import GPyBOModel
+    from .acquisition_functions import UCB
+    from .bo import BO
+
+    input_dim = bounds.shape[0]
+
+    kernel = GPy.kern.RBF(input_dim)
+    kernel.variance.set_prior(GPy.priors.LogGaussian(0.005, 0.5)) # log_prior()
+    model = GPyBOModel(kernel=kernel, num_mcmc=0, fix_noise=True)
+
+    acq = UCB(model)
+    bo = BO(f, model, acquisition_function=acq, n_iter=n_iter, bounds=bounds)
+    bo.run(do_plot=do_plot)
+    return bo
+
+
 def test_dngo_10_10_10_pe(f, bounds, n_iter, do_plot=False):
-    from src.bo import BO, random_hypercube_samples, vectorize
-    from src.acquisition_functions import EI, UCB
-    from src.bayesian_linear_regression import BayesianLinearRegression
-    from src.dngo import BOModel
-    from src.neural_network import TFModel
+    from .bo import BO, random_hypercube_samples, vectorize
+    from .acquisition_functions import EI, UCB
+    from .bayesian_linear_regression import BayesianLinearRegression
+    from .dngo import BOModel
+    from .neural_network import TFModel
 
     input_dim = bounds.shape[0]
     nn = TFModel(input_dim=input_dim, dim_basis=10, dim_h1=10, dim_h2=10, epochs=1000, batch_size=10)
@@ -64,11 +86,11 @@ def test_dngo_10_10_10_pe(f, bounds, n_iter, do_plot=False):
 
 def test_dngo_50_50_50_pe(f, bounds, n_iter, do_plot=False):
 
-    from src.bo import BO, random_hypercube_samples, vectorize
-    from src.acquisition_functions import EI, UCB
-    from src.bayesian_linear_regression import BayesianLinearRegression
-    from src.dngo import BOModel
-    from src.neural_network import TFModel
+    from .bo import BO, random_hypercube_samples, vectorize
+    from .acquisition_functions import EI, UCB
+    from .bayesian_linear_regression import BayesianLinearRegression
+    from .dngo import BOModel
+    from .neural_network import TFModel
 
     input_dim = bounds.shape[0]
     nn = TFModel(input_dim=input_dim, dim_basis=50, epochs=1000, batch_size=10)
@@ -82,11 +104,11 @@ def test_dngo_50_50_50_pe(f, bounds, n_iter, do_plot=False):
     return bo # bo.model.X, bo.model.Y, f_opt
 
 def test_dngo_50_50_50_marg(f, bounds, n_iter, do_plot=False):
-    from src.bo import BO, random_hypercube_samples, vectorize
-    from src.acquisition_functions import EI, UCB
-    from src.bayesian_linear_regression import BayesianLinearRegression
-    from src.dngo import BOModel
-    from src.neural_network import TFModel
+    from .bo import BO, random_hypercube_samples, vectorize
+    from .acquisition_functions import EI, UCB
+    from .bayesian_linear_regression import BayesianLinearRegression
+    from .dngo import BOModel
+    from .neural_network import TFModel
 
     input_dim = bounds.shape[0]
     nn = TFModel(input_dim=input_dim, dim_basis=50, epochs=1000, batch_size=10)
@@ -98,3 +120,33 @@ def test_dngo_50_50_50_marg(f, bounds, n_iter, do_plot=False):
     bo.run(do_plot=do_plot)
 
     return bo # bo.model.X, bo.model.Y, f_opt
+
+
+def test_multiple(bo_methods, n_iter=200):
+    """
+    Arguments:
+        bo_methods -- dictionary of functions returning BOBaseModel
+    """
+
+    f, bounds, f_opt = prepare_benchmark(Branin())
+
+    # Random baseline
+    rand_arg_his, rand_f_his = test_random_sample(f, bounds, n_iter)
+    
+    # Run models asynchronously
+    pool = mp.Pool()
+    results_dict = { name: pool.apply_async(method, [f, bounds, n_iter]) for name, method in bo_methods.items() }
+    bo_models_tuple = [ (name, result.get()) for name, result in results_dict.items() ]
+    bo_names = list(map(itemgetter(0), bo_models_tuple))
+    bo_models = list(map(itemgetter(1), bo_models_tuple))
+    ir = [acc_ir(bo.model.Y, f_opt) for bo in bo_models]
+
+    ir.insert(0, acc_ir(rand_f_his, f_opt))
+    bo_names.insert(0, "random")
+
+    sns.set_style("darkgrid")
+    plot_ir(ir)
+    plt.legend(bo_names)
+    plt.show()
+
+    return bo_models, bo_names, ir