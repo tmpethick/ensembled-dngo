bc4cb82191d8594c387b5a324020edbbaf6583e6
Thomas Pethick <tmpethick@gmail.com>
2018-08-16 16:13:47 +0200

    Fixes database merging

--- database_merger.py
+++ database_merger.py
@@ -8,7 +8,7 @@ try:
   remote_temp_database = pd.read_csv(conf['remote_temp_database'])
 
   df = pd.concat([remote_temp_database, local], sort=False)
-  df.groupby(["uuid"]).agg(lambda x: x.iloc[0])
+  df = df.groupby(["uuid"]).agg(lambda x: x.iloc[0]).reset_index()
 except FileNotFoundError:
   df = pd.read_csv(conf['remote_temp_database'])
 
